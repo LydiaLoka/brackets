84c1d1b4be09278106d9bc104c1bdee34332e19b
LydiaLoka <sevmek87@gmail.com>
2023-01-30 12:16:02 +0300

    fix: update js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const open = [ "(", "{", "["];
+  const close = [")" , "}", "]"];
+  const checkArr= [];
+  for (let i= 0; i < str.length; i++) {
+    if (open.includes(str[i])) {
+      checkArr.push(str[i]);
+    } else {
+      if(close.indexOf(str[i]) === open.indexOf(checkArr[checkArr.length-1])) {
+        checkArr.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return checkArr.length === 0; 
+    }
+ 
+  
